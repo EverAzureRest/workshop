e3bb26e126e45c86f5b179b999f0037249809e14
lorax79 <jordan.smith@powerhell.org>
2017-04-27 19:10:37 -0700

    First Release

---
+++ deploy-vm.ps1
@@ -0,0 +1,25 @@
+param(
+    $vnetResourceGroupName = "workshopSimpleVnet",
+    $vmResourceGroupName = "workshopVMs"
+    $deploymentlocation = "WestUS2"
+    $subscriptionName
+)
+
+$ParamHash = @{
+    'adminUsername' = "LocalAdmin";
+    'numberOfInstances' = 2
+    'vmNamePrefix' = "workshopvm0"
+    'virtualNetworkName' = "workshopvnet"
+    'virtualNetworkResourceGroup' = $vnetResourceGroupName
+    'subnetName' = "Subnet2"
+}
+
+Login-AzureRmAccount
+
+if $subscriptionName (
+    Get-AzurermSubscription -SubscriptionName $subscriptionName | Select-AzureRmSubscription
+)
+
+New-AzureRmResourceGroup -Name $vnetResourceGroupName -Location $deploymentlocation
+
+New-AzureRmResourceGroupDeployment -Name workshopvms -ResourceGroupName $vnetResourceGroupName -Mode Incremental -Templatefile .\windowsvm.json -TemplateParameterObject $ParamHash
---
+++ deploy-vnet.ps1
@@ -0,0 +1,26 @@
+param(
+    $vnetResourceGroupName = "workshopSimpleVnet",
+    $deploymentlocation = "WestUS2"
+    $subscriptionName
+)
+
+$ParamHash = @{
+    'vnetName' = "simpleVnet";
+    'vnetprefix' = "10.2.0.0/16";
+    'subnet1Name' = "Subnet1";
+    'subnet1Prefix' = "10.2.1.0/24";
+    'subnet2Name' = "Subnet2";
+    'subnet2Prefix' = "10.2.2.0/24";
+    'subnet3Name' = "Subnet3";
+    'subnet3Prefix' = "10.2.3.0/24"
+}
+
+Login-AzureRmAccount
+
+if $subscriptionName (
+    Get-AzurermSubscription -SubscriptionName $subscriptionName | Select-AzureRmSubscription
+)
+
+New-AzureRmResourceGroup -Name $vnetResourceGroupName -Location $deploymentlocation
+
+New-AzureRmResourceGroupDeployment -Name NATVM -ResourceGroupName $vnetResourceGroupName -Mode Incremental -Templatefile .\simplevnet.json -TemplateParameterObject $ParamHash
